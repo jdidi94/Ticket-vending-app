6a2cb32ee168356ca5a3521c8ff96032ad180cb6
jdidi94 <jdididaoud404@gmail.com>
2022-07-20 11:49:49 +0200

    addinig the order service

---
+++ ticketing/orders/src/app.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { json } from "body-parser";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+// import { createTicketRouter } from "./routes/new";
+import { errorHandler, NotFoundError, currentUser } from "@new-developers/work";
+// import { showTicketRouter } from "./routes/show";
+// import { fetchTickets } from "./routes/index";
+// import { updateTicketRouter } from "./routes/updates";
+
+const app = express();
+app.set("tust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+    // secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(currentUser);
+// console.log(process.env.NODE_ENV);
+// console.log(process.env.JWT_KEY);
+// app.use(createTicketRouter);
+// app.use(showTicketRouter);
+// app.use(fetchTickets);
+// app.use(updateTicketRouter);
+app.get("*", async () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+export { app };
---
+++ ticketing/orders/src/index.ts
@@ -0,0 +1,45 @@
+import Mongoose from "mongoose";
+import { natsWrapper } from "./nats-wrapper";
+import "express-async-errors";
+import { app } from "./app";
+const start = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI  should be defined");
+  }
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY  should be defined");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID  should be defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL  should be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID  should be defined");
+  }
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("pusblisher disconnected from NATS");
+      process.exit();
+    });
+    process.on("SIGNIT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    await Mongoose.connect(process.env.MONGO_URI);
+    console.log("connected to mongodb");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+app.listen(3000, () => {
+  console.log(" hi listening on 3000");
+});
+
+start();
