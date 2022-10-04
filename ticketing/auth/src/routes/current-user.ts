6ef1cea873114711da0a625ca8d28578f611e936
jdidi94 <jdididaoud404@gmail.com>
2022-10-04 15:55:27 +0200

    test cluster iinite

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -2,10 +2,8 @@ import express, { Request, Response } from "express";
 
 const router = express.Router();
 import { currentUser } from "@new-developers/work";
-
+console.log("happpy");
 router.get("/api/users/currentuser", currentUser, async (req, res) => {
-
-
   res.send({ currentUser: req.currentUser || null });
 });
 export { router as currentUserRouter };
