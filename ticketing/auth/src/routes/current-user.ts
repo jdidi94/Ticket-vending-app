4cb33a7958e99e8b2b5c1ce7ecd4fd903af550a8
jdidi94 <jdididaoud404@gmail.com>
2022-10-06 11:51:17 +0200

    ingress prod

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 
 const router = express.Router();
 import { currentUser } from "@new-developers/work";
-console.log("happpy");
+console.log("new files changes let's see what willl happen");
 router.get("/api/users/currentuser", currentUser, async (req, res) => {
   res.send({ currentUser: req.currentUser || null });
 });
